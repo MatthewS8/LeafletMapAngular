641fe042aeb28a8a23e78213e5a80405bb15140a
MatthewS8 <matthew-stefanelli@outlook.it>
2024-05-22 22:33:50 +0200

    first commit, created map component

---
+++ LeafletAngular/src/app/map/map.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent implements OnInit {
+  
+ private map: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  } 
+
+  
+
+
+  constructor() {
+   }
+  ngOnInit(): void {
+    this.initMap();
+   }
+}
